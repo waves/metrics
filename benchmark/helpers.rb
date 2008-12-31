e8c4c7d65248eb036822275e8647c11b623e3aa4
Matthew King <automatthew@gmail.com>
2008-12-30 20:31:49 -0600

    hi, steve

--- benchmark/helpers.rb
+++ benchmark/helpers.rb
@@ -1,4 +1,5 @@
 require "#{File.dirname(__FILE__)}/../helpers"
+require "#{File.dirname(__FILE__)}/steve"
 require "foundations/compact"
 
 module PathTemplate
--- benchmark/path_generation.rb
+++ benchmark/path_generation.rb
@@ -10,24 +10,24 @@ def generate(zipped, count, offset=50)
   end 
 end
 
-run_count, sample_count = 7, 100
+steve = Steve.new("Path generation with compilation")
 
-runs = []
-
-run_count.times do
-  samples = []
-  paths = Class.new( Waves::Resources::Paths ).new
-  sample_count.times do
-    samples << Benchmark.realtime do
-      generate(zipped, 500) { |t,a| paths.generate(t, a)  }
-    end
-  end
-  runs << samples
+def steve.sample_setup
+  @paths = Class.new( Waves::Resources::Paths ).new
 end
 
-puts "samples median: #{runs.map { |run| run.mean }.median }"
-puts "samples mean:  #{runs.map { |run| run.mean }.mean}"
-puts "samples stdev:  #{runs.map { |run| run.mean }.standard_deviation}"
+def steve.work(zipped)
+  generate(zipped, 50) { |t,a| @paths.generate(t, a)  }
+end
+
+
+steve.go(10, 70, zipped) { |sample| puts "mean: #{sample.mean}, sigma: #{sample.standard_deviation}" }
+
+puts "Runs mean: #{steve.runs_mean}"
+puts "Runs sigma: #{steve.runs_sigma}"
+
+steve.reset
+
 
 # puts "Path generation benchmarks"
 # 
