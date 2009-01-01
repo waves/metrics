f6a0ef5cdb474905d22cf3d64404c22bb4844ab0
Matthew King <automatthew@gmail.com>
2009-01-01 15:22:51 -0600

    now using automatthew-stevedore gem

--- benchmark/helpers.rb
+++ benchmark/helpers.rb
@@ -1,6 +1,8 @@
 require "#{File.dirname(__FILE__)}/../helpers"
-require "#{File.dirname(__FILE__)}/stevedore"
 require "foundations/compact"
+
+gem 'automatthew-stevedore'
+require 'stevedore'
 
 module PathTemplateGenerator
   
--- benchmark/path_generation.rb
+++ benchmark/path_generation.rb
@@ -1,27 +1,27 @@
 require "#{File.dirname(__FILE__)}/helpers"
 
-class PathGenPerf < Steve
+class PathGeneration < Steve
   include PathTemplateGenerator
   
   power 0.8
   sig_level 0.05
+  delta 0.001
   
   before do
-    # set up the Paths object
     @paths = Class.new( Waves::Resources::Paths ).new
   end
 
   before_measure do
     # generate an array of [template, args] before each measurement
-    @templates_and_args = templates_and_args(2000)
+    @templates_and_args = templates_and_args(1200)
   end
   
 end
 
-precompiled = PathGenPerf.new("New method, test paths precompiled") do
+
+precompiled = PathGeneration.new("New method, test paths precompiled") do
   before_sample do
-    # precompile the templates that will be used
-    @precompile = templates_and_args(2000)
+    @precompile = templates_and_args(1200)
     @precompile.each { |t,a| @paths.generate(t, a)  }
   end
   measure do
@@ -29,23 +29,23 @@ precompiled = PathGenPerf.new("New method, test paths precompiled") do
   end
 end
 
-compiled = PathGenPerf.new("New method, no precompilation") do
-  measure do
-    @templates_and_args.each { |t,a| @paths.generate(t, a)  }
-  end
-  after_measure do
-    @paths.compiled_paths.clear
-  end
-end
+# compiled = PathGeneration.new("New method, no precompilation") do
+#   measure do
+#     @templates_and_args.each { |t,a| @paths.generate(t, a)  }
+#   end
+#   after_measure do
+#     @paths.compiled_paths.clear
+#   end
+# end
 
-original = PathGenPerf.new("Old method") do
+original = PathGeneration.new("Old method") do
   measure do
     @templates_and_args.each { |t,a| @paths.original_generate(t, a)  }
   end
 end
 
 
-test_size = PathGenPerf.recommend_test_size(8, 64)
+test_size = PathGeneration.recommend_test_size(8, 64)
 
-PathGenPerf.compare_instances(*test_size)
+PathGeneration.compare_instances(*test_size)
 
